3ade22dbbdee3a53a21c318ba2671a40dcf8f3f3
Alexandr Kokorev <ak_78leon@mail.ru>
2017-09-09 17:51:20 +0600

    сделал минификацию кода

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,11 @@ var feedbackForm = feedbackPopup.querySelector("form");
 var userName = feedbackPopup.querySelector("[name=user-name]");
 var eMail = feedbackPopup.querySelector("[name=email]");
 
+var mapLink = document.querySelector(".map-link");
+
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
 
 
 
@@ -32,6 +37,24 @@ window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
     if (feedbackPopup.classList.contains("modal-show")) {
       feedbackPopup.classList.remove("modal-show");
+    }
+  }
+});
+
+mapLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
     }
   }
 });
